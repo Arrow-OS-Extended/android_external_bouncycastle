9d7707cd3a8c4957e25bc9369dc7966e006ae24f
Miguel Aranda <miguelaranda@google.com>
2025-02-18 09:40:13 -0800

    Restore deleted DER files to BC
    They all use the superclass anyways so I think it's harmless to keep these. And that way we prevent issues in which users are calling these directly.
    Change-Id: I3ca144f3bc01e273d5173a625e2d852711324ccb

---
+++ bcprov/src/main/java/org/bouncycastle/asn1/DERObjectIdentifier.java
@@ -0,0 +1,28 @@
+// Android-added: keep DER classes for backwards compatibility
+package org.bouncycastle.asn1;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ *
+ * @deprecated Use ASN1ObjectIdentifier instead of this,
+ */
+public class DERObjectIdentifier
+    extends ASN1ObjectIdentifier
+{
+    public DERObjectIdentifier(String identifier)
+    {
+        super(identifier);
+    }
+
+    DERObjectIdentifier(byte[] bytes)
+    {
+        super(new String(bytes, StandardCharsets.UTF_8));
+    }
+
+    DERObjectIdentifier(ASN1ObjectIdentifier oid, String branch)
+    {
+        super(oid, branch);
+    }
+}
+// Android-added: keep DER classes for backwards compatibility
